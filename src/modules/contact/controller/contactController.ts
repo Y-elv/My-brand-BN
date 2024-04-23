1e1e4b7a9169a6051b38f61b45831dc79244fa23
elvis mugisha <elvismugisha@elviss-MacBook-Pro.local>
2024-04-23 03:24:20 +0200

    adding tests

--- src/modules/contact/controller/contactController.ts
+++ src/modules/contact/controller/contactController.ts
@@ -1,19 +1,17 @@
 import { Request, Response } from "express";
 import sendEmail from "../repository/contactRepository";
 
- const SendMessage = async (
-  req: Request,
-  res: Response
-): Promise<void> => {
+const SendMessage = async (req: Request, res: Response): Promise<void> => {
   try {
     const { email, message, name } = req.body;
     await sendEmail(name, email, message);
-    res.status(200).send({ message: "Email sent successfully!" });
+    res.status(200).send({ status: true, message: "Email sent successfully!" });
   } catch (error) {
     console.error(error);
-    res
-      .status(500)
-      .send({ message: "An error occurred while sending the email." });
+    res.status(500).send({
+      status: false,
+      message: "An error occurred while sending the email.",
+    });
   }
 };
-export default SendMessage
+export default SendMessage;
---
+++ src/modules/contact/test/contact.spec.ts
@@ -0,0 +1,50 @@
+import app from "../../../index";
+import chaiHttp from "chai-http";
+import chai, { expect } from "chai";
+import sinon from "sinon";
+import sendEmail from "../repository/contactRepository";
+
+chai.use(chaiHttp);
+const router = () => chai.request(app);
+
+describe("Contact Test Cases", () => {
+  it("Should be able to send a message", (done) => {
+    router()
+      .post("/api/v1/contact")
+      .send({
+        name: "Elvis",
+        email: "testuser@gmail.com",
+        message: "Hiiii",
+      })
+      .end((error, response) => {
+        expect(response.body).to.be.a("object");
+        expect(response.body).to.have.property("status");
+        expect(response.body).to.have.property("message");
+        expect(response.body.status).to.equal(true);
+        expect(response.body.message).to.equal("Email sent successfully!");
+
+        done(error);
+      });
+  });
+
+  it("Should n't be able to send a message", (done) => {
+    router()
+      .post("/api/v1/contact")
+      .send({
+        name: "Elvis",
+        email: "testuser@gmail.com",
+        message: "Hiiii",
+      })
+      .end((error, response) => {
+        expect(response.body).to.be.a("object");
+        expect(response.body).to.have.property("status");
+        expect(response.body).to.have.property("message");
+        expect(response.body.status).to.equal(false);
+        expect(response.body.message).to.equal(
+          "An error occurred while sending the email."
+        );
+
+        done(error);
+      });
+  });
+});
