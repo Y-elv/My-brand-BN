4ca2d623409a541b1684a5f9c4d3acb6cc775b1a
elvis mugisha <elvismugisha@elviss-MacBook-Pro.local>
2024-04-28 14:04:59 +0200

    adding all functionalities in swagger

--- docs/blog/index.ts
+++ docs/blog/index.ts
@@ -7,7 +7,7 @@ const blog = {
       summary: "Create A Blog",
       description: "Create A Blog",
       parameters: [
-          {
+        {
           in: "body",
           name: "body",
           schema: {
@@ -33,8 +33,98 @@ const blog = {
           required: true,
           description: "Image file to upload for the blog",
         },
-      
       ],
+      consumes: ["multipart/form-data"],
+      produces: ["application/json"],
+      responses,
+      security: [
+        {
+          JWT: [],
+        },
+      ],
+    },
+  },
+  "/blog/getAllBlog": {
+    get: {
+      tags: ["Blogs"],
+      summary: "Get all Blogs ",
+      description: "Get all Blogs ",
+
+      consumes: ["application/json"],
+      produces: ["application/json"],
+      responses,
+    },
+  },
+  "/blog/deleteBlog/{blogId}": {
+    delete: {
+      tags: ["Blogs"],
+      summary: "Delete Blog ",
+      description: "Delete Blog ",
+      parameters: [
+        {
+          in: "path",
+          name: "blogId",
+          required: true,
+          schema: {
+            type: "string",
+          },
+          description: "The ID of the Blog to be deleted",
+        },
+      ],
+
+      consumes: ["application/json"],
+      produces: ["application/json"],
+      responses,
+      security: [
+        {
+          JWT: [],
+        },
+      ],
+    },
+  },
+  "/blog/updateBlog/{blogId}": {
+    patch: {
+      tags: ["Blogs"],
+      summary: "Update Blog",
+      description: "Update Blog ",
+      parameters: [
+        {
+          in: "path",
+          name: "blogId",
+          required: true,
+          schema: {
+            type: "string",
+          },
+          description: "The ID of the Comment to be retrieved ",
+        },
+        {
+          in: "body",
+          name: "body",
+          schema: {
+            type: "object",
+            properties: {
+              name: {
+                type: "string",
+                description: "Name of a blog",
+              },
+              description: {
+                type: "string",
+                description: "Description of the blog",
+              },
+            },
+            required: ["name", "description"],
+          },
+          required: true,
+        },
+        {
+          in: "formData",
+          name: "image",
+          type: "file",
+          required: true,
+          description: "Image file to upload for the blog",
+        },
+      ],
+
       consumes: ["multipart/form-data"],
       produces: ["application/json"],
       responses,
--- docs/comment/index.ts
+++ docs/comment/index.ts
@@ -39,5 +39,75 @@ const comment = {
       ],
     },
   },
+  "/comment/getAllComment": {
+    get: {
+      tags: ["Comment"],
+      summary: "Get all Comments ",
+      description: "Get all Comments ",
+
+      consumes: ["application/json"],
+      produces: ["application/json"],
+      responses,
+      security: [
+        {
+          JWT: [],
+        },
+      ],
+    },
+  },
+  "/comment/deleteComment/{commentId}": {
+    delete: {
+      tags: ["Comment"],
+      summary: "Delete Comment ",
+      description: "Delete Comment ",
+      parameters: [
+        {
+          in: "path",
+          name: "commentId",
+          required: true,
+          schema: {
+            type: "string",
+          },
+          description: "The ID of the Comment to be deleted",
+        },
+      ],
+
+      consumes: ["application/json"],
+      produces: ["application/json"],
+      responses,
+      security: [
+        {
+          JWT: [],
+        },
+      ],
+    },
+  },
+  "/comment/getComment/{blogId}": {
+    get: {
+      tags: ["Comment"],
+      summary: "Get Comment By Id",
+      description: "Get Comment By Id ",
+      parameters: [
+        {
+          in: "path",
+          name: "blogId",
+          required: true,
+          schema: {
+            type: "string",
+          },
+          description: "The ID of the Comment to be retrieved ",
+        },
+      ],
+
+      consumes: ["application/json"],
+      produces: ["application/json"],
+      responses,
+      security: [
+        {
+          JWT: [],
+        },
+      ],
+    },
+  },
 };
 export default comment;
